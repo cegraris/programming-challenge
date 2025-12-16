b26089cd0b101ac56ab3fcc1b1309d7f83d7dd13
Jiahao Wu <jiahao.wu@exxcellent.de>
2025-12-16 10:23:55 +0100

    feat: Most basic implementation that can pass the basic test

--- src/main/java/de/exxcellent/challenge/model/Weather.java
+++ src/main/java/de/exxcellent/challenge/model/Weather.java
@@ -1,5 +1,6 @@
 package de.exxcellent.challenge.model;
 
+import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,33 +12,48 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Weather {
+    @CsvBindByName(column = "Day")
     private Integer day;
 
+    @CsvBindByName(column = "MxT")
     private Integer maxTemperature;
 
+    @CsvBindByName(column = "MnT")
     private Integer minTemperature;
 
+    @CsvBindByName(column = "AvT")
     private Integer avgTemperature;
 
+    @CsvBindByName(column = "AvDP")
     private Double avgDewPoint;
 
+    @CsvBindByName(column = "1HrP TPcpn")
     private Double oneHourPrecipitation;
 
+    @CsvBindByName(column = "TPcpn")
     private Double totalPrecipitation;
 
+    @CsvBindByName(column = "PDir")
     private Integer prevailingWindDirection;
 
+    @CsvBindByName(column = "AvSp")
     private Double avgWindSpeed;
 
+    @CsvBindByName(column = "Dir")
     private Integer windDirection;
 
+    @CsvBindByName(column = "MxS")
     private Double maxWindSpeed;
 
+    @CsvBindByName(column = "SkyC")
     private Double skyCover;
 
+    @CsvBindByName(column = "MxR")
     private Integer maxRelativeHumidity;
 
+    @CsvBindByName(column = "Mn")
     private Integer minRelativeHumidity;
 
+    @CsvBindByName(column = "R AvSLP")
     private Double avgSeaLevelPressure;
 }
--- src/main/java/de/exxcellent/challenge/reader/serviceImpl/TableFileReaderCsvImpl.java
+++ src/main/java/de/exxcellent/challenge/reader/serviceImpl/TableFileReaderCsvImpl.java
@@ -1,8 +1,11 @@
 package de.exxcellent.challenge.reader.serviceImpl;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import de.exxcellent.challenge.reader.service.TableFileReader;
 
 import java.io.Reader;
+import java.util.Iterator;
 import java.util.Optional;
 
 /**
@@ -12,18 +15,30 @@ import java.util.Optional;
  */
 public class TableFileReaderCsvImpl<T> implements TableFileReader<T> {
 
+    private final Reader reader;
+    private final Iterator<T> iterator;
+
     public TableFileReaderCsvImpl(Reader reader, Class<T> clazz) {
-        // TODO: initialize components
+        this.reader = reader;
+
+        CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(reader)
+                .withType(clazz)
+                .withIgnoreLeadingWhiteSpace(true)
+                .build();
+
+        this.iterator = csvToBean.iterator();
     }
 
     @Override
     public Optional<T> read() throws Exception {
-        // TODO: read and convert next CSV record
+        if (iterator.hasNext()) {
+            return Optional.of(iterator.next());
+        }
         return Optional.empty();
     }
 
     @Override
     public void close() throws Exception {
-        // TODO: Implement closing underlying resources
+        reader.close();
     }
 }
