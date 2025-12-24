8a01cb8a122e1ec876322391d0b61152e8c7cec8
Jiahao Wu <jiahao.wu@exxcellent.de>
2025-12-24 12:44:11 +0100

    feat: Implement WeatherAnalyser

--- src/main/java/de/exxcellent/challenge/analyser/serviceImpl/WeatherAnalyserImpl.java
+++ src/main/java/de/exxcellent/challenge/analyser/serviceImpl/WeatherAnalyserImpl.java
@@ -1,10 +1,13 @@
 package de.exxcellent.challenge.analyser.serviceImpl;
 
 import de.exxcellent.challenge.analyser.service.WeatherAnalyser;
+import de.exxcellent.challenge.exception.AppException;
 import de.exxcellent.challenge.model.Weather;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 /**
@@ -14,7 +17,51 @@ import java.util.stream.Stream;
 public class WeatherAnalyserImpl implements WeatherAnalyser {
     @Override
     public Set<Integer> findDayOfSmallestTemperatureSpread(Stream<Weather> weathers) {
-        return null;
+        Set<Integer> seenDays = new HashSet<>();
+        Set<Integer> result = new HashSet<>();
+        AtomicInteger minSpread = new AtomicInteger(Integer.MAX_VALUE);
+
+        weathers
+                .peek(w -> validateWeather(w, seenDays))
+                .forEach(w -> {
+                    int spread = w.getMaxTemperature() - w.getMinTemperature();
+                    if (spread < minSpread.get()) {
+                        minSpread.set(spread);
+                        result.clear();
+                        result.add(w.getDay());
+                    } else if (spread == minSpread.get()) {
+                        result.add(w.getDay());
+                    }
+                });
+
+        if (result.isEmpty()) {
+            throw AppException.analysisFailed("No valid data after filtering");
+        }
+        return result;
+    }
+
+    /**
+     * Validates a single weather record.
+     * <p>Checks that:
+     * <ul>
+     *   <li>min temperature does not exceed max temperature</li>
+     *   <li>day is not duplicated in the dataset</li>
+     * </ul>
+     *
+     * @param w        the weather record to validate
+     * @param seenDays seenDays set of days already processed, used for duplicate detection
+     * @throws AppException if validation fails
+     */
+    private void validateWeather(Weather w, Set<Integer> seenDays) {
+        if (w.getMinTemperature() > w.getMaxTemperature()) {
+            throw AppException.analysisFailed(
+                    String.format("Day %d: min temperature (%d) exceeds max temperature (%d)",
+                            w.getDay(), w.getMinTemperature(), w.getMaxTemperature()));
+        }
+        if (!seenDays.add(w.getDay())) {
+            throw AppException.analysisFailed(
+                    String.format("Duplicate day: %d", w.getDay()));
+        }
     }
 
 }
